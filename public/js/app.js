c5b5bc2b7e52ab56beaae58e0525d4d98514a915
Avalon7 <derrickyinx@gmail.com>
2017-07-27 13:28:56 +1000

    make google map full screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
 
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
-var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'ngMap']);
