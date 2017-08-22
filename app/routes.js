64f0189910d674ed50457e77bbf93138ecfa00a0
Avalon7 <derrickyinx@gmail.com>
2017-08-22 15:44:56 +1000

    fix marker animation bug

--- app/routes.js
+++ app/routes.js
@@ -7,8 +7,8 @@ module.exports = function(app) {
 
     // GET Routes
     // --------------------------------------------------------
-    // Retrieve records for all users in the db
-    app.get('/users', function(req, res){
+    // Retrieve records in db
+    app.get('/singleEvent', function(req, res){
 
         // Uses Mongoose schema to run the search (empty conditions)
         var query = User.find({});
@@ -24,18 +24,18 @@ module.exports = function(app) {
     // POST Routes
     // --------------------------------------------------------
     // Provides method for saving new users in the db
-    app.post('/users', function(req, res){
+    app.post('/singleEvent', function(req, res){
 
         // Creates a new User based on the Mongoose schema and the post bo.dy
-        var newuser = new User(req.body);
+        // var newuser = new User(req.body);
 
         // New User is saved in the db.
-        newuser.save(function(err){
-            if(err)
-                res.send(err);
+        // newuser.save(function(err){
+        //     if(err)
+        //         res.send(err);
 
-            // If no errors are found, it responds with a JSON of the new user
-            res.json(req.body);
-        });
+        //     // If no errors are found, it responds with a JSON of the new user
+        //     res.json(req.body);
+        // });
     });
 };  
