51f35891b9a763df44db09e3320251d390620505
Chun Yu Chan <chunyu@Chun-Yus-MacBook-Pro.local>
2017-08-30 15:57:10 +1000

    Modified progress menu, added task information and progress cirluar

--- public/js/singleEventCtrl.js
+++ public/js/singleEventCtrl.js
@@ -9,13 +9,18 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 	var directionsService;
 	var stepDisplay;
 
+
 	var position;
 	var marker = [];
 	var polyline = [];
 	var poly2 = [];
 	var poly = null;
 	var timerHandle = [];
-	var timerHandle = [];
+	var startLoc = new Array();
+	var facilitiesMarker = new Array();
+	var endLoc;
+	var startLocation = new Array();
+	var endLocation = new Array();
 
 	var speed = 0.000005, wait = 1;
 	var infowindow = null;
@@ -26,6 +31,7 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 
 	NgMap.getMap("map").then(function(map){
 		vm.map = map;
+		vm.map.setZoom(10);
 	});
 
 	//put a marker by clicking mouse
@@ -40,8 +46,7 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 			});
 		}
 		//display the marker info
-		var htmlElement = "	<div><div><p id=\"event-setting-header\">Single Event Setting</p></div> " + 
-		"<div><button class=\"button continue-btn ripple\" ng-click=\"vm.setDataField()\">" + "Set event data" + "</button></div></div>"
+		var htmlElement = "	<div><div><p id=\"event-setting-header\">Single Event Setting</p></div><div><button class=\"button continue-btn ripple\" ng-click=\"vm.setDataField()\">" + "Set event data" + "</button></div></div>"
 		// var htmlElement = "<showTag></showTag>"
 		//need to compile 
 		var compiled = $compile(htmlElement)($scope)
@@ -53,13 +58,12 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 		vm.marker.addListener('click', function($scope){
 			vm.marker.infoWin.open(vm.map, vm.marker);
 		});
-		//clear onclick event in marker
 		google.maps.event.clearListeners(vm.map, 'click');
 	}
 
 	vm.setDataField = function(){
 		//change center view
-		vm.map.setZoom(18);
+		// vm.map.setZoom(25);
 		vm.map.setCenter(vm.marker.position);
 		// $scope.open = function(){
 			
@@ -113,6 +117,16 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 		}
   	}
 
+  	vm.progrssMenuOpen = function () {
+	    ngDialog.open({ 
+	      template: 'eventProgress.html',
+	      overlay: false,
+	      showClose: false,
+	      scope: $scope,
+	      className: 'ngdialog-theme-default progress-menu draggable'       
+	    });
+    };
+
 	// now start the simulation
 	vm.startSingleEvent = function(){
 		// close factor menu
@@ -121,7 +135,7 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 		vm.progrssMenuOpen();
 
 		// console.log($scope.factor);
-		vm.map.setZoom(16);
+		// vm.map.setZoom(16);
 		vm.map.setCenter(vm.marker.position);
 
 		// post data to back-end
@@ -141,22 +155,28 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 		//receive facilities location from server and put markers on map
 		//using fake data right now
 		//wait back end implementation
-		vm.facilities = [];
-		vm.destinations  = [];
-		var facility1 = {lat: -34.4105585, lng: 150.8783824};
-		var facility2 = {lat: -34.4853985, lng: 150.872664};
-		vm.facilities.push(facility1);
-		vm.facilities.push(facility2);
+		// vm.facilities = [];
+		// vm.destinations  = [];
+		// var facility1 = {lat: -34.4105585, lng: 150.8783824};
+		// var facility2 = {lat: -34.4853985, lng: 150.872664};
+		// vm.facilities.push(facility1);
+		// vm.facilities.push(facility2);
+		startLoc[0] = 'Sydney';
+		startLoc[1] = 'Hyams Beach';
+		endLoc = 'University of Wollongong';
 		
-		for(var i = 0; i < vm.facilities.length; ++i){
-			vm.destinations[i] = new google.maps.Marker({
-				position: vm.facilities[i],
-				map: vm.map, 
-				animation: google.maps.Animation.DROP
-			});
-		}
+		// for(var i = 0; i < endLoc.length; ++i){
+		// 	facilitiesMarker[i] = new google.maps.Marker({
+		// 		position: startLoc[i],
+		// 		map: vm.map,
+		// 		animation: google.maps.Animation.DROP
+		// 	});
+		// }
 		
 		//set the routes between startloc and endloc
+
+		receiveEventTask();
+
 		setRoutes();
 	} 
 
@@ -171,7 +191,7 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 		        scope: $scope,
 		        preserveScope: true,
 		        controller: function($scope) {
-			}
+			},
 		});
 	};
 
@@ -185,19 +205,7 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
     	$mdDialog.cancel();
   	}
 
-  	vm.progrssMenuOpen = function () {
-        ngDialog.open({ 
-        	template: 'eventProgress.html',
-        	overlay: false,
-        	showClose: false,
-        	scope: $scope,
-        	className: 'ngdialog-theme-default progress-menu draggable'     	
-        });
-    };
-
   	function createMarker(latlng, label, html) {
-	// alert("createMarker("+latlng+","+label+","+html+","+color+")");
-	    var contentString = '<b>'+label+'</b><br>'+html;
 	    var marker = new google.maps.Marker({
 	        position: latlng,
 	        map: vm.map,
@@ -206,17 +214,13 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 	        });
 	        marker.myname = label;
 
-
-	    google.maps.event.addListener(marker, 'click', function() {
-	        infowindow.setContent(contentString); 
-	        infowindow.open(map,marker);
-	        });
 	    return marker;
 	}  
 
 
   	function setRoutes(){
   		var directionDisplay = new Array();
+  		for(var i = 0; i < startLoc.length; ++i){
   		var rendererOptions = {
   			map: vm.map,
   			suppressMarkers : true,
@@ -225,85 +229,86 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
   		directionsService = new google.maps.DirectionsService();
 
   		var travelMode = google.maps.DirectionsTravelMode.DRIVING;
-  		vm.requests = [];
-  		for(var i = 0; i < vm.destinations.length; ++i){
+  		//vm.requests = [];
+  		
   			vm.request = {
-  				origin: vm.marker.position,
-  				destination: vm.destinations[i].position,
+  				origin: startLoc[i],
+  				destination: endLoc,
   				travelMode: travelMode
   			};
   			directionsService.route(vm.request, makeRouteCallback(i, directionDisplay[i]));
   		}
   		
   		function makeRouteCallback(routeNum, dip){
-  		if(polyline[routeNum] && (polyline[routeNum].getMap() != null)){
-  			startAnimation(routeNum);
-  			return;
-  		}
-  		return function(response, status){
-  			if(status == google.maps.DirectionsStatus.OK){
-  				var bounds = new google.maps.LatLngBounds();
-  				var route = response.routes[0];
-  				vm.marker.position = new Object();
-  				vm.destinations[routeNum] = new Object();
+	  		if(polyline[routeNum] && (polyline[routeNum].getMap() != null)){
+	  			startAnimation(routeNum);
+	  			return;
+	  		}
+	  		return function(response, status){
+	  			if(status == google.maps.DirectionsStatus.OK){
 
-  				polyline[routeNum] = new google.maps.Polyline({
-  				path: [],
-            	strokeColor: '#FFFF00',
-            	strokeWeight: 3 
-            	});
-  				poly2[routeNum] = new google.maps.Polyline({
-	            path: [],
-	            strokeColor: '#FFFF00',
-	            strokeWeight: 3
-	            });    
+	  				var bounds = new google.maps.LatLngBounds();
+	  				var route = response.routes[0];
+	  				startLocation[routeNum] = new Object();
+	  				endLocation[routeNum] = new Object();
 
-	            //fir each route, display summary information
-  				var path = response.routes[0].overview_path;
-	            var legs = response.routes[0].legs;
+	  				polyline[routeNum] = new google.maps.Polyline({
+	  				path: [],
+	            	strokeColor: '#FFFF00',
+	            	strokeWeight: 3 
+	            	});
+	  				poly2[routeNum] = new google.maps.Polyline({
+		            path: [],
+		            strokeColor: '#FFFF00',
+		            strokeWeight: 3
+		            });    
+
+		            //fir each route, display summary information
+	  				var path = response.routes[0].overview_path;
+		            var legs = response.routes[0].legs;
 
 
-	            disp = new google.maps.DirectionsRenderer(rendererOptions);     
-	            disp.setMap(vm.map);
-	            disp.setDirections(response);
+		            disp = new google.maps.DirectionsRenderer(rendererOptions);     
+		            disp.setMap(vm.map);
+		            disp.setDirections(response);
 
-	            for (i=0;i<legs.length;i++) {
-	              if (i == 0) { 
-	                vm.marker.location = legs[i].start_location;
-	                vm.marker.location.address = legs[i].start_address;
-	                // marker = google.maps.Marker({map:map,position: startLocation.latlng});
-	                marker[routeNum] = createMarker(legs[i].start_location,"start",legs[i].start_address,"green");
-	              }
-	              vm.destinations[routeNum].latlng = legs[i].end_location;
-	              vm.destinations[routeNum].address = legs[i].end_address;
-	              var steps = legs[i].steps;
+		            //create resources markers
+		            for (i=0;i<legs.length;i++) {
+		              if (i == 0) { 
+		                startLocation[routeNum].latlng = legs[i].start_location;
+		                startLocation[routeNum].address = legs[i].start_address;
+		                // marker = google.maps.Marker({map:map,position: startLocation.latlng});
+		                marker[routeNum] = createMarker(legs[i].start_location,"start",legs[i].start_address,"green");
+		              }
+		              endLocation[routeNum].latlng = legs[i].end_location;
+		              endLocation[routeNum].address = legs[i].end_address;
+		              var steps = legs[i].steps;
 
-	              for (j=0;j<steps.length;j++) {
-	                var nextSegment = steps[j].path;                
-	                var nextSegment = steps[j].path;
+		              for (j=0;j<steps.length;j++) {
+		                var nextSegment = steps[j].path;                
+		                var nextSegment = steps[j].path;
 
-	                for (k=0;k<nextSegment.length;k++) {
-	                    polyline[routeNum].getPath().push(nextSegment[k]);
-	                    //bounds.extend(nextSegment[k]);
-	                }
+		                for (k=0;k<nextSegment.length;k++) {
+		                    polyline[routeNum].getPath().push(nextSegment[k]);
+		                    //bounds.extend(nextSegment[k]);
+		                }
 
-	              }
-            	}
+		              }
+	            	}
 
-            	polyline[routeNum].setMap(vm.map);
-		         
-		        //map.fitBounds(bounds);
-		        startAnimation(routeNum);  
-
-  			}
-  		}
-  	}
+	            	polyline[routeNum].setMap(vm.map);		         
+			    //map.fitBounds(bounds);
+		        	startAnimation(routeNum); 
+	  			}
+	  			
+	  		}
+  		}	
   	}
 
   	var eol = [];
   	var lastVertex = 1;
   	var stepnum=0;
-    var step = 5; // 5; // metres
+    var step = 50; // 5; // metres
     var tick = 100; // milliseconds
 
   	function updatePoly(i,d) {
@@ -319,37 +324,63 @@ app.controller('mainContrl', function(NgMap, $compile, $scope, $mdDialog, $http,
 	        }
 	            poly2[i].getPath().insertAt(poly2[i].getPath().getLength(),polyline[i].GetPointAtDistance(d));
 	    } else {
-	        poly2[i].getPath().insertAt(poly2[i].getPath().getLength(),vm.destinations[i].latlng);
+	        poly2[i].getPath().insertAt(poly2[i].getPath().getLength(),endLocation[i].latlng);
 	    }
 	 }
   	function animate(index,d) {
+  		// console.log("animate function");
 	   if (d>eol[index]) {
-
 	      // marker[index].setPosition(endLocation[index].latlng);
-	      vm.marker.setPosition(vm.destinations[index].latlng);
+	      marker[index].setPosition(endLocation[index].latlng);
 	      return;
 	   }
 	    var p = polyline[index].GetPointAtDistance(d);
 
 	    //map.panTo(p);
 	    // marker[index].setPosition(p);
-	    vm.marker.setPosition(vm.destinations[index].latlng);
+	    marker[index].setPosition(p);
 	    updatePoly(index,d);
-	    // timerHandle[index] = setTimeout("animate("+index+","+(d+step)+")", tick);
-	    $timeout(animate(index, (d + step)), tick);
+	    //timerHandle[index] = setTimeout($scope.$apply(animate(index,(d+step))), tick);
+	    //timerHandle[index] =  $timeout($scope.$apply(animate(index, (d + step))), tick);
 	}
 
   	function startAnimation(index){
-  		if(timerHandle[index])
-  			clearTimeout(timerHandle[index]); 
-  		eol[index] = polyline[index].Distance();
-  		vm.map.setCenter(polyline[index].getPath().getAt(0));
 
+  		console.log("start marker animation");
+  		if(timerHandle[index])
+  			$timeout.cancel(timerHandle[index]);
+  		// console.log(polyline[index]);
+  		eol[index] = polyline[index].Distance();
+  		// vm.map.setCenter(polyline[index].getPath().getAt(0));
+  		//vm.map.setZoom(20);
   		poly2[index] = new google.maps.Polyline({path: [polyline[index].getPath().getAt(0)],
   						strokeColor:"#FFFF00", strokeWeight:3});
-		// timerHandle[index] = setTimeout("animate("+index+",50)", 2000);
-		$timeout(animate(index, 50), 2000);
+		//timerHandle[index] = setTimeout($scope.$apply(animate(index,50)), 2000);
+		//timerHandle[index] = $timeout($scope.$apply(animate(index, 50)), 2000);
+		//timerHandle[index] = animate(index, 50);
   	}
+
+    /* this is a mockup function at the moment, it's regarding to receive task from server */
+    function receiveEventTask(){
+
+      vm.services = [];
+      //for loop to receive type of resources needed
+        //push()
+        vm.services =
+        [{
+          resource: 'Police car',
+          number: 2
+        },{
+          resource: 'Ambulance',
+          number: 3
+        }];
+
+        vm.totalResource = 0;
+        for(var i = 0; i < vm.services.length; i++){
+	        vm.totalResource += vm.services[i].number;
+		}
+      //end for loop
+    }
 });
 
 app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
@@ -361,7 +392,7 @@ app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
         $mdSidenav(componentId).toggle();
       };
     }
-  });
+});
 
 
 app.directive("showForm", function(){
