ffc1d4ac20e0d583631f3e859595b96905ee79f1
Avalon7 <derrickyinx@gmail.com>
2017-10-11 22:04:01 +1100

    add map to facility window

--- public/js/facilityCtrl.js
+++ public/js/facilityCtrl.js
@@ -6,4 +6,27 @@ app.controller('facilityCtrl', function(NgMap, $compile, $scope, $mdDialog, $htt
 	facilityVm.selectedFacility = angular.fromJson(accessData);
 
 	console.log(facilityVm.selectedFacility);
+
+	NgMap.getMap("map").then(function(map){
+    facilityVm.map = map;
+    facilityVm.map.setZoom(14);
+    // show search box as defualt
+    // facilityVm.searchExtend();
+
+
+
+    //
+    updateGPS = function(){
+    	$http({
+    		method	: 'POST',
+	    	url		: '',
+	    	headers	: { 'Content-Type': 'application/json' },
+	    	data 	: {
+	    				sim_id: facilityVm.selectedFacility.sim_id
+	    	}
+    	}).then(function success(response)){
+
+    	}
+
+    }
 });
